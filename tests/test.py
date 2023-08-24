da1c5a2fab3abbf245be10ae0677115b6b493f47
Ove Aursland <jeircul@mailbox.org>
2023-08-24 22:38:26 +0200

    Updated tests.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,37 @@
 import os
+import unittest
 from firipy import FiriAPI
 
-
 token = os.environ['API_KEY_FIRI']
-f = FiriAPI(token)
-print(f.btc_Address())
+
+
+class TestFiriAPI(unittest.TestCase):
+    def test_get_time(self):
+        fg = FiriAPI(token)
+        time = fg.time()
+        self.assertIsNotNone(time)
+        self.assertIsInstance(time, dict)
+
+    def test_history_transactions(self):
+        fg = FiriAPI(token)
+        transactions = fg.history_transactions()
+        self.assertIsNotNone(transactions)
+        self.assertIsInstance(transactions, list)
+        self.assertGreater(len(transactions), 0)
+
+    def test_history_transactions_year(self):
+        fg = FiriAPI(token)
+        transactions = fg.history_transactions_year(2023)
+        self.assertIsNotNone(transactions)
+        self.assertIsInstance(transactions, list)
+        self.assertGreater(len(transactions), 0)
+
+    def test_history_transactions_month_year(self):
+        fg = FiriAPI(token)
+        transactions = fg.history_transactions_month_year(6, 2023)
+        self.assertIsNotNone(transactions)
+        self.assertIsInstance(transactions, list)
+        self.assertGreater(len(transactions), 0)
+
+if __name__ == '__main__':
+    unittest.main()
